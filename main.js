efd40c941c7acc7c0d62138a2a5e02257345b07a
MJ-2009 <januskiewiczmason60@gmail.com>
2025-11-20 19:44:06 +0000

    got the start down got help from AI and teacher rember to cite them alr put in comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /* Main game file: main.js */
-/* Game: [Your Game Name Here] */
-/* Authors: [Your Name(s) Here] */
-/* Description: [Short description of your game here] */
-/* Citations: [List any resources, libraries, tutorials, etc you used here] 
+/* Game: Paddle smaker */
+/* Authors: Mason Januskiewicz] */
+/* Description: use paddle to smack ball that starts at top of screen */
+/* Citations: most likely going to use ai I am putting this here ahead of time */
 /* Note: If you use significant AI help you should cite that here as well */
 /* including summaries of prompts and/or interactions you had with the AI */
 /* In addition, of course, any AI-generated code should be clearly maked */
@@ -10,15 +10,28 @@
 /* auto-complete it maye be impractical to mark every line, which is why you */
 /* should also include a summary here */
 
-
 import "./style.css";
 
-import { GameInterface } from 'simple-canvas-library';
+import { GameInterface } from "simple-canvas-library";
 
 let gi = new GameInterface();
 
 /* Variables: Top-Level variables defined here are used to hold game state */
+//Paddle
+let paddleSizeSet = false;
+let paddlex = gi.width / 2; //starting position
+let Paddley = gi.height + 30; // fixed hieght
+//are subject to change as needed these are just arbitary numbers
+let paddlewidth = 80;
+let paddleheight = 10;
+let paddlespeed = 8;
 
+//falling object
+// falling object
+let objectx = Math.random() * gi.width;
+let objecty = 0;
+let objectspeed = 25;
+let objectradius = 5;
 
 /* Drawing Functions */
 
@@ -27,26 +40,48 @@ that will be called in sequence each frame. It's a good idea to do
 one function per each object you are putting on screen, and you
 may then want to break your drawing function down into sub-functions
 to make it easier to read/follow */
-gi.addDrawing(
-  function ({ ctx, width, height, elapsed, stepTime }) {
-    // Your drawing code here...    
+gi.addDrawing(function drawpaddle({ ctx, width, height, elapsed, stepTime }) {
+  //got helpt from teacher will put in citation later
+  if (!paddleSizeSet) {
+    //set paddle size based on screen size
+    paddlewidth = width / 10;
+    paddleheight = height / 40;
+    // set position
+    Paddley = height - paddleheight - 10;
+    paddlex = (width - paddlewidth) / 2;
+    paddleSizeSet = true;
   }
-)
+  // Your drawing code here...
+  ctx.fillStyle = "blue";
+  ctx.fillRect(paddlex, Paddley, paddlewidth, paddleheight);
+});
+
+gi.addDrawing(function drawobject({ ctx, width, height, elapsed, stepTime }) {
+//was ai I didnt write if (!isFinite(objectx) || isNaN(objectx)) { it was helping me fix a bug where the ball would continuously randomize its position
+  if (!isFinite(objectx) || isNaN(objectx)) {
+    objectx = Math.random() * width;
+  }
+  ctx.fillStyle = "green";
+  ctx.beginPath();
+  ctx.arc(objectx, objecty, objectradius, 0, Math.PI * 2);
+  ctx.fill();
+  //update object position
+  objecty += objectspeed * (stepTime / 1000);
+  //reset object if it goes off screen
+  if (objecty > height) {
+    objecty = 0;
+    objectx = Math.random() * width;
+  }
+});
 
 /* Input Handlers */
 
 /* Example: Mouse click handler (you can change to handle 
 any type of event -- keydown, mousemove, etc) */
 
-gi.addEventListener(
-  "click",
-  function ({ event, x, y }) {
-    // Your click handling code here...
-  }
-)
-
+gi.addHandler("click", function ({ event, x, y }) {
+  // Your click handling code here...
+});
 
 /* Run the game */
 gi.run();
-
-
